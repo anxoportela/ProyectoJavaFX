3a934bad62e41ec08e566d0520f048e18d892be6
llllJokerllll <55234541+llllJokerllll@users.noreply.github.com>
2020-03-21 20:43:29 +0100

    update by Joker

--- src/com/proyecto/javafx/app/JuegosController.java
+++ src/com/proyecto/javafx/app/JuegosController.java
@@ -1,6 +1,8 @@
 package com.proyecto.javafx.app;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.ResourceBundle;
 
 import com.proyecto.javafx.modelo.Persona;
@@ -13,9 +15,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
@@ -23,47 +27,40 @@ import javafx.scene.input.MouseEvent;
 public class JuegosController implements Initializable {
 	
 	@FXML
-	private Button btnAgregar;
-	
-	@FXML
-	private Button btnModificar;
-	
-	@FXML
-	private Button btnEliminar;
-	
-	@FXML
-	private TextField txtTitulo;
-	
-	@FXML
-	private TextField txtGenero;
-	
-	@FXML
-	private TextField txtPlataforma;
-	
-	@FXML
-	private ComboBox<String> comboBox;
+    private TextField txtTitulo;
 
-	@FXML
-	private TableView<Persona> tblPersonas;
+    @FXML
+    private ComboBox<String> comboBoxGenero;
+
+    @FXML
+    private ComboBox<String> comboBoxPlataforma;
+
+    @FXML
+    private Button  btnGuardar;
+
+    @FXML
+    private Button btnSalir;
+
+    @FXML 
+    private RadioButton rdbSi;
+
+    @FXML 
+    private RadioButton rdbNo;
+
+    @FXML 
+    private ToggleGroup grp1;
 	
-	@FXML
-	private TableColumn colTitulo;
+	private ObservableList<Juego> juegos;
 	
-	@FXML
-	private TableColumn colGenero;
-	
-	@FXML
-	private TableColumn colPlataforma;
-	
-	private ObservableList<Persona> personas;
+	private JuegoDAO juegoDAO;
 	
 	@FXML
 	private void agregarJuego(ActionEvent event) {
 		
 		try {
-			Persona p = new Persona(this.txtTitulo.getText(), this.txtGenero.getText(), Integer.parseInt(this.txtPlataforma.getText()));
-			this.personas.add(p);
-			this.tblPersonas.setItems(this.personas);
+			Juego j = new Juego(this.txtTitulo.getText(), this.comboBoxGenero.getValue(), this.comboBoxPlataforma.getValue(), grp1.getSelectedToggle().getUserData().toString());
+			this.juegos.add(j);
+			this.tblJuegos.setItems(this.juegos);
 		} catch (NumberFormatException e) {
 			Alert alert = new Alert(AlertType.WARNING);
 			alert.setHeaderText(null);
@@ -75,65 +72,43 @@ public class JuegosController implements Initializable {
 	}
 	
 	@FXML
-	private void seleccionar(MouseEvent event) {
-		Persona p = this.tblPersonas.getSelectionModel().getSelectedItem();
-		if (p != null) {
-			this.txtTitulo.setText(p.getNombreString());
-			this.txtGenero.setText(p.getApellidoString());
-			this.txtPlataforma.setText(String.valueOf(p.getEdad()));
-		}
+    private void modificarJuego(ActionEvent event) {
+        Juego j = this.tblJuegos.getSelectionModel().getSelectedItem();
+        if (j == null) {
+            Alert alert = new Alert(AlertType.WARNING);
+            alert.setHeaderText(null);
+            alert.setTitle("Error");
+            alert.setContentText("No hay nada seleccionado");
+            alert.showAndWait();
+        } else {
+        	
+        }
+        
 	}
 	
 	@FXML
-	private void modificarJuego(ActionEvent event) {
-		Persona p = this.tblPersonas.getSelectionModel().getSelectedItem();
-		if (p == null) {
-			Alert alert = new Alert(AlertType.WARNING);
-			alert.setHeaderText(null);
-			alert.setTitle("Error");
-			alert.setContentText("No hay nada seleccionado");
-			alert.showAndWait();
-		} else {			
-			try {
-				Persona m = new Persona(this.txtTitulo.getText(), this.txtGenero.getText(), Integer.parseInt(this.txtPlataforma.getText()));
-				
-				if (this.personas.contains(m)) {
-					Alert alert = new Alert(AlertType.WARNING);
-					alert.setHeaderText(null);
-					alert.setTitle("Error");
-					alert.setContentText("La persona ya existe");
-					alert.showAndWait();
-				} else {
-					p.setNombreString(m.getNombreString());
-					p.setApellidoString(m.getApellidoString());
-					p.setEdad(m.getEdad());
-					this.tblPersonas.refresh();
-				}
-				
-			} catch (NumberFormatException e) {
-				Alert alert = new Alert(AlertType.WARNING);
-				alert.setHeaderText(null);
-				alert.setTitle("Error");
-				alert.setContentText("Alg�n dato con formato incorrecto");
-				alert.showAndWait();
-			}			
+	private void seleccionar(MouseEvent event) {
+		Juego j = this.tblJuegos.getSelectionModel().getSelectedItem();
+		if (j != null) {
+			this.txtTitulo.setText(j.getTitulo());
+			this.txtGenero.setText(j.getGenero());
+			this.txtPlataforma.setText(j.getPlataforma());
+			this.txtMultijugador.setText(j.isMultijugador());
 		}
-
-
 	}
 	
 	@FXML
 	private void eliminarJuego(ActionEvent event) {
-		Persona p = this.tblPersonas.getSelectionModel().getSelectedItem();
-		if (p == null) {
+		Juego j = this.tblJuegos.getSelectionModel().getSelectedItem();
+		if (j == null) {
 			Alert alert = new Alert(AlertType.WARNING);
 			alert.setHeaderText(null);
 			alert.setTitle("Error");
 			alert.setContentText("No hay nada seleccionado");
 			alert.showAndWait();
 		} else {
-			this.personas.remove(p);
-			this.tblPersonas.setItems(this.personas);
+			this.juegos.remove(j);
+			this.tblJuegos.setItems(this.juegos);
 		}
 
 	}
@@ -141,12 +116,19 @@ public class JuegosController implements Initializable {
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		
-		this.personas = FXCollections.observableArrayList();
-		
-		colTitulo.setCellValueFactory(new PropertyValueFactory("nombreString"));
-		colGenero.setCellValueFactory(new PropertyValueFactory("apellidoString"));
-		colPlataforma.setCellValueFactory(new PropertyValueFactory("edad"));
-		
+		this.juegos = FXCollections.observableArrayList();
+        this.tblJuegos.setItems(this.juegos);
+        this.juegoDAO = new JuegoDAO();
+        ArrayList<Juego> lista = (ArrayList) this.juegoDAO.obter();
+        Iterator var5 = lista.iterator();
+
+        while (var5.hasNext()) {
+            Juego j = (Juego) var5.next();
+            this.juegos.add(j);
+        }
+
+        this.tblJuegos.refresh();
 	}
 
 }
+
