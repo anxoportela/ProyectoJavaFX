42ee4a8310352f434216abe9c073cf21bdb7f630
Anxo Portela-insua Blanco <anxoportela@gmail.com>
2020-03-21 22:29:32 +0100

    Update

--- src/com/proyecto/javafx/app/JuegosDiagController.java
+++ src/com/proyecto/javafx/app/JuegosDiagController.java
@@ -17,92 +17,90 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 public class JuegosDiagController implements Initializable {
-	
+
 	@FXML
 	private TextField txtTitulo;
-	
+
 	@FXML
 	private ComboBox<String> comboBoxGenero;
-	
+
 	@FXML
-	private ComboBox<String> comboBoxPlataforma;		
-    
+	private ComboBox<String> comboBoxPlataforma;
+
 	@FXML
-	private Button  btnGuardar;
-	
+	private Button btnGuardar;
+
 	@FXML
 	private Button btnSalir;
-    
-    @FXML 
-    private RadioButton rdbSi;
-    
-    @FXML 
-    private RadioButton rdbNo;
-    
-    @FXML 
-    private ToggleGroup grp1;
-    
-    private Juego juego;    
-    private ObservableList<Juego> juegos;
-    
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        
-        
-    }
 
-    public void inicializarAtributos(ObservableList<Juego> juegos) {
-        this.juegos = juegos;       
-    }
-    
-    public void inicializarAtributos(ObservableList<Juego> juegos, Juego j) {
+	@FXML
+	private RadioButton rdbSi;
+
+	@FXML
+	private RadioButton rdbNo;
+
+	@FXML
+	private ToggleGroup grp1;
+
+	private Juego juego;
+	private JuegoDAO juegoDAO;
+	private ObservableList<Juego> juegos;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+	}
+
+	public void inicializarAtributos(ObservableList<Juego> juegos) {
+		this.juegos = juegos;
+	}
+
+	public void inicializarAtributos(ObservableList<Juego> juegos, Juego j) {
 		this.juegos = juegos;
 		this.juego = j;
 		this.txtTitulo.setText(j.getTitulo());
 	}
-    
-    @FXML
-    private void salir(ActionEvent event) {
-        this.juego = null;
-        Stage stage = (Stage) this.btnGuardar.getScene().getWindow();
-        stage.close();
-    }
-    
-    @FXML
-    private void guardar(ActionEvent event) {
-        try {
-			String multi;
-			if (rdbSi.isSelected()) {
-				multi = "Sí";
-			} else {
-				multi = "No";
-			}
-            Juego j = new Juego(this.txtTitulo.getText(), this.comboBoxGenero.getValue(), this.comboBoxPlataforma.getValue(), multi);
-            if (this.juegos.contains(j)) {
-                Alert alert = new Alert(AlertType.WARNING);
-                alert.setHeaderText(null);
-                alert.setTitle("Error");
-                alert.setContentText("El juego ya existe");
-                alert.showAndWait();
-            } else {
-                this.juego = j;
-                Alert alert = new Alert(AlertType.INFORMATION);
-                alert.setHeaderText(null);
-                alert.setTitle("Perfecto");
-                alert.setContentText("Juego añadido correctamente");
-                alert.showAndWait();
-            }
-            
-        } catch (NumberFormatException e) {
-            Alert alert = new Alert(AlertType.ERROR);
-            alert.setHeaderText(null);
-            alert.setTitle("Error");
-            alert.setContentText("Datos incorrectos");
-            alert.showAndWait();
-        }
-    }
-    
-    public Juego getJuego() {
+
+	@FXML
+	private void salir(ActionEvent event) {
+		this.juego = null;
+		Stage stage = (Stage) this.btnGuardar.getScene().getWindow();
+		stage.close();
+	}
+
+	@FXML
+	private void guardar(ActionEvent event) {
+		String multi;
+		Alert alert;
+		if (rdbSi.isSelected()) {
+			multi = "Sí";
+		} else {
+			multi = "No";
+		}
+		Juego j = new Juego(this.txtTitulo.getText(), this.comboBoxGenero.getValue(),
+				this.comboBoxPlataforma.getValue(), multi);
+		if (!this.juegos.contains(j)) {
+			this.juego.setTitulo(this.txtTitulo.getText());
+			this.juego.setGenero(this.comboBoxGenero.getValue());
+			this.juego.setPlataforma(this.comboBoxPlataforma.getValue());
+			this.juego.setMultijugador(multi);
+			alert = new Alert(AlertType.INFORMATION);
+			alert.setHeaderText((String) null);
+			alert.setTitle("Información");
+			alert.setContentText("Modificado correctamente");
+			alert.showAndWait();
+			Stage stage = (Stage) this.btnGuardar.getScene().getWindow();
+			stage.close();
+		} else {
+			alert = new Alert(AlertType.ERROR);
+			alert.setHeaderText((String) null);
+			alert.setTitle("Erro");
+			alert.setContentText("El juego ya existe");
+			alert.showAndWait();
+		}
+	}
+
+	public Juego getJuego() {
 		return this.juego;
 	}
 
